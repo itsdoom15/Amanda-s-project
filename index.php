e1e5875e5eb751f7267196842f1e269b1a40978b
Jordane Beckford <jbeckford67@gmail.com>
2016-01-15 12:09:23 -0500

    index

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Amanda's art</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+<div class="nav">
+
+  <ul>
+
+  	 <a href="index.php"><li>Home</li></a>|
+  	 <a href="gallery.php"><li>Gallery</li></a>|
+  	 <a href="about me.php"><li>About me</li></a>|
+  	 <a href="contact me.php"><li>Contact me</li></a>
+
+  </ul>
+
+
+</div>
+
+<hr>
+
+
+<div class="title">
+
+<h1>Amanda's Page</h1>
+
+</div>
+
+<div class="">
+
+<img height="160px" width="160px" src="">
+
+</div>
+
+<div class="bio">
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+</div>
+
+<br>
+
+<div class="display">
+
+<img src="">
+
+<img src="">
+
+<img src="">
+
+</div>
+
+
+</body>
+
+</html>
